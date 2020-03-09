2ee0879c379ad6508757b9013ad00395c0810e24
adepamungkas <ade.pamungkas9@gmail.com>
2020-03-10 06:34:28 +0700

    Updates

---
+++ precache-manifest.4679ed30d0e7c7f33e246519b91ca45a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7735bd633f53a4dc75cb01aa96e8e4af",
+    "url": "/product/index.html"
+  },
+  {
+    "revision": "1aa18dcd58721a0c0317",
+    "url": "/product/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cd988618f64f781c0bea",
+    "url": "/product/static/js/2.f57a186b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/product/static/js/2.f57a186b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa18dcd58721a0c0317",
+    "url": "/product/static/js/main.b7bb7764.chunk.js"
+  },
+  {
+    "revision": "0c5c746e51a0e39e43ca",
+    "url": "/product/static/js/runtime-main.668a8c4f.js"
+  }
+]);
